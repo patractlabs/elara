2a9000906dad4e771fec16d25801a10f9a8206f2
toxotguo <toxotguo@gmail.com>
2020-11-11 15:49:20 +0800

    fix consumer offset bug

--- packages/stat/kafka/consumer.js
+++ packages/stat/kafka/consumer.js
@@ -1,25 +1,36 @@
 const config = global.config = require('../config/index')();
 const { logger } = require('../src/lib/log')
 const stat = require('../src/api/stat')
+const kafka = require('kafka-node');
+const Offset = kafka.Offset;
+const client = new kafka.KafkaClient({ kafkaHost: config.kafka.kafkaHost });
+const offset = new Offset(client);
+const consumer = new kafka.Consumer(
+    client,
+    [
+        { topic: config.kafka.topic, partition: 0 }
+    ],
+    {
+        autoCommit: true
+    }
+);
 
-var kafka = require('kafka-node'),
-    Consumer = kafka.Consumer,
-    client = new kafka.KafkaClient({ kafkaHost: config.kafka.kafkaHost }),
-    consumer = new Consumer(
-        client,
-        [
-            { topic: config.kafka.topic, partition: 0 }
-        ],
-        {
-            autoCommit: true
-        }
-    );
+console.log('consumer start');
 
-consumer.on('error', function (err) {
-    logger.error('kafka error', err)
-})
+consumer.on('error', function (error) {
+    logger.error('error', error)
+});
+consumer.on('offsetOutOfRange', function (topic) {
+    logger.error('offsetOutOfRange', topic)
+    offset.fetch([topic], function (err, offsets) {
+        let min = Math.min.apply(null, offsets[topic.topic][topic.partition])
+        consumer.setOffset(topic.topic, topic.partition, min)
+        logger.info('setOffset', topic.topic, topic.partition, min)
+    })
+});
 
 consumer.on('message', function (message) {
+    console.log(message)
     try {
         switch (message.key) {
             case 'request': {
