9f828810204d6a8d4795dd8418afeda87543f4f1
toxotguo <toxotguo@gmail.com>
2021-01-19 16:23:13 +0800

    env test env

---
+++ packages/api/config/env/test.env.js
@@ -0,0 +1,27 @@
+process.env.DEBUG = process.env.DEBUG || 'dev-errer:*'
+
+module.exports = {
+    name: 'elara',
+    port: 7003,
+    messengers: {
+        'polkadot': ['ws://127.0.0.1:7005'],
+        'kusama': ['ws://127.0.0.1:7005'],
+        'jupiter': ['ws://127.0.0.1:7005'],
+        'rococo': ['ws://127.0.0.1:7005'],
+        'darwinia': ['ws://127.0.0.1:7005'],
+        'dock': ['ws://127.0.0.1:7005'],
+        'edgeware': ['ws://127.0.0.1:7005'],
+        'kulupu': ['ws://127.0.0.1:7005'],
+        'nodle': ['ws://127.0.0.1:7005'],
+        'plasm': ['ws://127.0.0.1:7005'],
+        'stafi': ['ws://127.0.0.1:7005'],
+        'mandala': ['ws://127.0.0.1:7005']
+    },
+    kafka: {
+        'kafkaHost': '127.0.0.1:9092',
+        'topic': 'elara-dev',
+        'sasl': { mechanism: 'plain', username: '***', password: '***' }
+    },
+    statServer: '127.0.0.1:7002',
+    pool: 10
+}
---
+++ packages/messenger/config/env/test.env.js
@@ -0,0 +1,71 @@
+process.env.DEBUG = process.env.DEBUG || 'dev-errer:*'
+
+module.exports = {
+    name: 'messenger',
+    port: 7005,
+    chain: {
+        'polkadot': {
+            'rpc': ['https://polkadot.elara.patract.io'],
+            'ws': ['wss://polkadot.elara.patract.io'],
+            'processors': ['node', 'cache']//处理器列表
+        },
+        'kusama': {
+            'rpc': ['https://kusama.elara.patract.io'],
+            'ws': ['wss://kusama.elara.patract.io'],
+            'processors': ['node', 'cache']
+        },
+        'jupiter': {
+            'rpc': ['https://jupiter.elara.patract.io'],
+            'ws': ['wss://jupiter.elara.patract.io'],
+            'processors': ['node', 'cache']
+        },
+        'rococo': {
+            'rpc': ['https://rococo.elara.patract.io'],
+            'ws': ['wss://rococo.elara.patract.io'],
+            'processors': ['node', 'cache']
+        },
+        'darwinia': {
+            'rpc': ['https://darwinia.elara.patract.io'],
+            'ws': ['wss://darwinia.elara.patract.io'],
+            'processors': ['node', 'cache']
+        },
+        'dock': {
+            'rpc': ['https://dock.elara.patract.io'],
+            'ws': ['wss://dock.elara.patract.io'],
+            'processors': ['node', 'cache']
+        },
+        'edgeware': {
+            'rpc': ['https://edgeware.elara..patract.io'],
+            'ws': ['wss://edgeware.elara..patract.io'],
+            'processors': ['node', 'cache']
+        },
+        'kulupu': {
+            'rpc': ['https://kulupu.elara.patract.io'],
+            'ws': ['wss://kulupu.elara.patract.io'],
+            'processors': ['node', 'cache']
+        },
+        'nodle': {
+            'rpc': ['https://nodle.elara.patract.io'],
+            'ws': ['wss://nodle.elara.patract.io'],
+            'processors': ['node', 'cache']
+        },
+        'plasm': {
+            'rpc': ['https://plasm.elara.patract.io'],
+            'ws': ['wss://plasm.elara.patract.io'],
+            'processors': ['node', 'cache']
+        },
+        'stafi': {
+            'rpc': ['https://stafi.elara.patract.io'],
+            'ws': ['wss://stafi.elara.patract.io'],
+            'processors': ['node', 'cache']
+        },
+        'mandala': {
+            'rpc': ['https://mandala.elara.patract.io'],
+            'ws': ['wss://mandala.elara.patract.io'],
+            'processors': ['node', 'cache']
+        }
+    },
+    history: {
+    },
+    pool: 10
+}
